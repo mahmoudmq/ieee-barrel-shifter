306856751d5f78f83c99a85c257fe2958a32c5c0
Mahmoud Magdy <102227604+mahmoudmq@users.noreply.github.com>
2026-09-09 12:39:49 +0300

    Refactor input/output pin assignments and comments
    Updated comments for clarity and modified assignment for uio_oe.

--- src/project.v
+++ src/project.v
@@ -18,12 +18,10 @@ module tt_um_npu (
 
     barrel_shifter_8bit shift_inst (ui_in, uio_in[2:0], uo_out);
 
-    // Not used input pins assigned to 0
-    assign uio_in[7:3] = 0;
 
-  // All output pins must be assigned. If not used, assign to 0.
+  // All input/output pins must be assigned. If not used, assign to 0.
   assign uio_out = 0;
-  assign uio_oe  = 0;
+assign uio_oe = {8{&uio_in[7:3]}};
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
